9f523675ca90db93836d047a1d27180d15bc206e
Piotr Stapp <piotr.stapp@finai.com>
2017-08-14 12:42:15 +0200

    Enable relative paths in configuration

--- src/dotnetTestExplorer.ts
+++ src/dotnetTestExplorer.ts
@@ -21,8 +21,12 @@ export class DotnetTestExplorer implements vscode.TreeDataProvider<vscode.TreeIt
 
         try {
             const testProjectPath = Utility.getConfiguration().get<string>("testProjectPath");
-            this.cwd = testProjectPath ? testProjectPath : vscode.workspace.rootPath;
-            const testStrings = Executor.execSync("dotnet test -t", this. cwd)
+            let testProjectFullPath = testProjectPath ? testProjectPath : vscode.workspace.rootPath;
+            if(!path.isAbsolute(testProjectFullPath)){
+                testProjectFullPath = path.resolve(vscode.workspace.rootPath, testProjectPath);
+            }
+            this.cwd  = testProjectFullPath;
+            const testStrings = Executor.execSync("dotnet test -t", this.cwd)
                 .split(/[\r\n]+/g).filter((item) => item);
             const index = testStrings.indexOf("The following Tests are available:");
             if (index > -1) {
