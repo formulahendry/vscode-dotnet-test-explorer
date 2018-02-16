40dfeab133c1af11eb853b2cafb9c061daa40a83
Janaka A <contact@janaka.co.uk>
2018-02-16 21:37:34 +0000

    Add test dotnet project

---
+++ test/test-dotnet-project/README.md
@@ -0,0 +1,3 @@
+# test dotnet project
+
+This is a dotnet core console app container xunit tests used for testing the extension.
---
+++ test/test-dotnet-project/tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+        [Fact]
+        public void Test2()
+        {
+
+        }
+        [Fact]
+        public void Test3()
+        {
+
+        }
+    }
+}
