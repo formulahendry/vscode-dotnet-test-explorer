8ff497fd95b7fad3bdc8aa8853c4661f522046ce
Janaka A <contact@janaka.co.uk>
2018-02-18 02:15:47 +0000

    Add unit tests to test project

--- test/test-dotnet-project/tests/UnitTest1.cs
+++ test/test-dotnet-project/tests/UnitTest1.cs
@@ -6,9 +6,27 @@ namespace tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestAdd()
         {
             Assert.Equal(5, add(2, 3));
+        }
+
+        [Fact]
+        public void TestMultiply()
+        {
+            Assert.Equal(9, multiply(3, 3));
+        }
+
+        [Fact]
+        public void TestMultiplyInvalid()
+        {
+            Assert.Equal(12, multiply(3, 4));
+        }
+
+        int multiply(int a, int b)
+        {
+            if (b == 4) { b = 2; };
+            return a * b;
         }
 
         int add(int a, int b)
