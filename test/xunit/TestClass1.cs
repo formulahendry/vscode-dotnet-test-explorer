aa08fd154df4d4716ad33089f796d82a14a3094d
stefanforsberg <cykelkatalogen@popkram.com>
2018-03-03 08:42:29 +0100

    Added testprojects for mstest nunit and xunit

---
+++ test/xunit/TestClass1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace XunitTests
+{
+
+    public class TestClass1
+    {
+        [Fact]
+        public void Pass()
+        {
+            (1+1).ShouldBe(2);
+        }
+
+        [Fact]
+        public void AnotherPass()
+        {
+            (1+1).ShouldBe(2);
+        }
+
+        [Fact]
+        public void Fail()
+        {
+            (1+1).ShouldBe(2);
+        }        
+    }
+}
