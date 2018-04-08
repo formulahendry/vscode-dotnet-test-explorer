b28270af401afb93bc65c36fd83b00dcbc8e7697
Stefan Forsberg <cykelkatalogen@popkram.com>
2018-04-08 18:29:41 +0200

    Lint fix

--- test/index.ts
+++ test/index.ts
@@ -9,13 +9,13 @@
 // host can call to run the tests. The test runner is expected to use console.log
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
-
-var testRunner = require("vscode/lib/testrunner");
+// tslint:disable-next-line:no-var-requires
+const testRunner = require("vscode/lib/testrunner");
 
 // You can directly control Mocha options by uncommenting the following lines
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
 testRunner.configure({
-    ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
+    ui: "tdd", 		 // the TDD UI is being used in extension.test.ts (suite, test, etc.)
     useColors: true, // colored output from test results
 });
 
