eb96bff7cbe4475caf834ac286eb21f8be3e3c3d
Stefan Forsberg <cykelkatalogen@popkram.com>
2018-04-08 18:33:22 +0200

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -9,6 +9,7 @@
 // host can call to run the tests. The test runner is expected to use console.log
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
+
 // tslint:disable-next-line:no-var-requires
 const testRunner = require("vscode/lib/testrunner");
 
