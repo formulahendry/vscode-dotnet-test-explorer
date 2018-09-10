084b1baafc7cfb0368ad7df17ca7d7a9264d3b47
MGenuit <42111466+Mgenuit@users.noreply.github.com>
2018-09-10 09:26:06 +0200

    Move changes to NUnit tests to their own file

--- test/nunit/TestClass1.cs
+++ test/nunit/TestClass1.cs
@@ -5,36 +5,24 @@ using Shouldly;
 namespace NunitTests
 {
     [TestFixture]
-    public class SingleTestFixture
+    public class TestClass1
     {
         [Test]
         public void Pass()
         {
-            (1 + 1).ShouldBe(2);
+            (1+1).ShouldBe(2);
         }
 
         [Test]
         public void AnotherPass()
         {
-            (1 + 1).ShouldBe(2);
+            (1+1).ShouldBe(2);
         }
 
         [Test]
         public void Fail()
         {
-            (1 + 1).ShouldBe(2);
-        }
-        [TestCase]
-        public void SingleTestCaseAtribute()
-        {
-            (1 + 1).ShouldBe(2);
-        }
-        [TestCase("First")]
-        [TestCase("Second")]
-        public void MultipleTestCaseAtributes(string number)
-        {
-            (1 + 1).ShouldBe(2);
-        }
-
+            (1+1).ShouldBe(2);
+        }        
     }
 }
--- test/nunit/TestClass2.cs
+++ test/nunit/TestClass2.cs
@@ -4,47 +4,32 @@ using Shouldly;
 
 namespace NunitTests
 {
-    [TestFixture("First")]
-    [TestFixture("Second")]
-    public class MultipleTestFixtures
+    [TestFixture]
+    public class TestClass2
     {
-        public MultipleTestFixtures(string x)
-        {
-        }
-
         [Test]
         public void Pass()
         {
-            (1 + 1).ShouldBe(2);
+            (1+1).ShouldBe(2);
         }
 
         [Test]
         public void Pass2()
         {
-            (1 + 1).ShouldBe(2);
+            (1+1).ShouldBe(2);
         }
 
         [Test]
         public void Fail()
         {
-            (1 + 1).ShouldBe(3);
-        }
-        [TestCase]
-        public void SingleTestCaseAtribute()
-        {
-            (1 + 1).ShouldBe(2);
-        }
-        [TestCase("First")]
-        [TestCase("Second")]
-        public void MultipleTestCaseAtributes(string number)
-        {
-            (1 + 1).ShouldBe(2);
-        }
+            (1+1).ShouldBe(3);
+        }   
+
         [Test]
         [Ignore("Skipped")]
         public void SkippedTest()
         {
-            (1 + 1).ShouldBe(3);
-        }
+            (1+1).ShouldBe(3);
+        }             
     }
 }
