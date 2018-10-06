ffc955922afa3916f2b380ef5c0fb53c975d61aa
Stefan Forsberg <cykelkatalogen@popkram.com>
2018-10-06 20:57:01 +0200

    Update appinsightsclient

--- src/appInsightsClient.ts
+++ src/appInsightsClient.ts
@@ -6,11 +6,12 @@ export class AppInsightsClient {
 
     public static EnableTelemetry = Utility.getConfiguration().get<boolean>("enableTelemetry");
 
-    public static sendEvent(eventName: string, properties?: { [key: string]: string; }, measurments?: { [key: string]: number; }): void {
+    public static sendEvent(eventName: string, properties?: { [key: string]: string; }, measurements?: { [key: string]: number; }): void {
         if (this.EnableTelemetry) {
-            this._client.trackEvent(eventName, properties, measurments);
+            this._client.trackEvent({name: eventName, properties, measurements});
         }
     }
 
-    private static _client = appInsights.getClient("d4c27ff4-02a1-41ed-bb90-2816cd675ab8");
+    private static _config = appInsights.setup("d4c27ff4-02a1-41ed-bb90-2816cd675ab8");
+    private static _client = appInsights.defaultClient;
 }
