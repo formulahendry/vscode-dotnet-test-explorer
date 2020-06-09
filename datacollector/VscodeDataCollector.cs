3e5bcdac7dcc0704418ebeca6e81e991c6084853
Georg Wicke <georg.wicke@rwth-aachen.de>
2020-06-09 20:40:40 +0200

    Started implementing a DataCollector

---
+++ datacollector/VscodeDataCollector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using VstestDataCollector = Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection.DataCollector;
+
+namespace VscodeTestExplorer.DataCollector
+{
+    [DataCollectorFriendlyName("VscodeDataCollector")]
+    [DataCollectorTypeUri("this://is/a/random/path/that/vstest/apparently/expects/whatever/VscodeDataCollector")]
+    public class VscodeDataCollector : VstestDataCollector
+    {
+        public override void Initialize(XmlElement configurationElement, 
+            DataCollectionEvents events, 
+            DataCollectionSink dataSink, 
+            DataCollectionLogger logger, 
+            DataCollectionEnvironmentContext environmentContext)
+        {
+            Console.WriteLine("Hello World");
+            events.TestCaseEnd += (sender, e) => Console.WriteLine($"{e.TestCaseName}: {e.TestOutcome}");
+
+            // int port = int.Parse(Environment.GetEnvironmentVariable("VSCODE_DOTNET_TEST_EXPLORER_DATA_COLLECTOR_PORT"));
+            // Console.WriteLine(port);
+        }
+    }
+}
