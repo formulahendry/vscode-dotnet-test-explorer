b84e3a131e7e184c4e3c1525ac9d7859f3cf02c6
Georg Wicke <georg.wicke@rwth-aachen.de>
2020-06-10 00:11:31 +0200

    Send some slightly less nonsense data

--- datacollector/VscodeDataCollector.cs
+++ datacollector/VscodeDataCollector.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using System.Threading;
+using System.IO;
+using System.Text;
+using System.Net.Sockets;
+using System;
 using System.Xml;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
 using VstestDataCollector = Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection.DataCollector;
@@ -9,17 +13,23 @@ namespace VscodeTestExplorer.DataCollector
     [DataCollectorTypeUri("this://is/a/random/path/that/vstest/apparently/expects/whatever/VscodeDataCollector")]
     public class VscodeDataCollector : VstestDataCollector
     {
-        public override void Initialize(XmlElement configurationElement, 
-            DataCollectionEvents events, 
-            DataCollectionSink dataSink, 
-            DataCollectionLogger logger, 
+        int port;
+        void SendString(string str)
+        {
+            using TcpClient client = new TcpClient("localhost", port);
+            using var writer = new StreamWriter(client.GetStream(), Encoding.UTF8);
+            writer.Write(str);
+        }
+        public override void Initialize(XmlElement configurationElement,
+            DataCollectionEvents events,
+            DataCollectionSink dataSink,
+            DataCollectionLogger logger,
             DataCollectionEnvironmentContext environmentContext)
         {
-            Console.WriteLine("Hello World");
-            events.TestCaseEnd += (sender, e) => Console.WriteLine($"{e.TestCaseName}: {e.TestOutcome}");
+            events.TestCaseEnd += (sender, e) => SendString($"{e.TestCaseName}: {e.TestOutcome}");
 
-            // int port = int.Parse(Environment.GetEnvironmentVariable("VSCODE_DOTNET_TEST_EXPLORER_DATA_COLLECTOR_PORT"));
-            // Console.WriteLine(port);
+            port = int.Parse(Environment.GetEnvironmentVariable("VSCODE_DOTNET_TEST_EXPLORER_PORT"));
+            Console.WriteLine($"Data collector initialized; writing to port {port}.");
         }
     }
 }
