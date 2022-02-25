c26eeafe3a7c69eae8ed5dcfe53e171f09abe382
Georg Wicke-Arndt <georg.wicke@rwth-aachen.de>
2022-02-25 02:10:44 +0100

    Hacked test execution together

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 "use strict";
 
-import { exec } from "child_process";
+import { exec, execFile } from "child_process";
+import { mkdtemp, rmdir, unlink } from "fs/promises";
+import { tmpdir } from "os";
+import path = require("path");
 import * as vscode from "vscode";
 import { AppInsights } from "./appInsights";
 import { AppInsightsClient } from "./appInsightsClient";
@@ -17,6 +20,7 @@ import { StatusBar } from "./statusBar";
 import { TestCommands } from "./testCommands";
 import { TestDirectories } from "./testDirectories";
 import { TestNode } from "./testNode";
+import { parseResults } from "./testResultsFile";
 import { TestStatusCodeLensProvider } from "./testStatusCodeLensProvider";
 import { Utility } from "./utility";
 import { Watch } from "./watch";
@@ -34,20 +38,31 @@ export function activate(context: vscode.ExtensionContext) {
 
     const controller = vscode.tests.createTestController("helloWorldTests", "Hello World Tests");
 
+    const env = {
+        ...process.env,
+        DOTNET_CLI_UI_LANGUAGE: "en",
+        VSTEST_HOST_DEBUG: "0",
+    };
+
     for (const folder of vscode.workspace.workspaceFolders) {
-        const cwd = folder.uri.fsPath;
-        exec("dotnet test --list-tests --verbosity=quiet", { cwd }, (error, stdout, stderr) => {
-            console.log(cwd);
+        const options = {
+            cwd: folder.uri.fsPath,
+            env
+        }
+        execFile("dotnet", ["test", "--list-tests", "--verbosity=quiet"], options, (error, stdout, stderr) => {
+            console.log(stdout);
             if (error) {
+                console.error(error);
                 // some error happened
-                // TODO: log it
+                // TODO: log it (properly)
                 return;
             }
 
             const lines = stdout.split(/\n\r?|\r/);
             const rawTests = lines.filter(line => /^    /.test(line));
             const parsedTestNames = rawTests.map(x => parseTestName(x.trim()));
-            const rootTree = mergeSingleItemTrees(buildTree(parsedTestNames));
+            // const rootTree = mergeSingleItemTrees(buildTree(parsedTestNames));
+            const rootTree = buildTree(parsedTestNames);
 
             // convert the tree into tests
             const generateNode = (tree: ITestTreeNode) => {
@@ -56,7 +71,7 @@ export function activate(context: vscode.ExtensionContext) {
                     treeNode.children.add(generateNode(subTree));
                 }
                 for (const test of tree.tests) {
-                    treeNode.children.add(controller.createTestItem(test, test));
+                    treeNode.children.add(controller.createTestItem(tree.fullName + "." + test, test));
                 }
 
                 return treeNode;
@@ -72,38 +87,70 @@ export function activate(context: vscode.ExtensionContext) {
         const run = controller.createTestRun(request, "My test run", true);
         const wait = () => new Promise(resolve => setTimeout(resolve, 1000));
 
-        let tests = request.include ?? controller.items;
-        tests.forEach(test => {
-            run.enqueued(test);
-        })
-        await wait();
-        tests.forEach(test => {
-            run.started(test);
-        })
-        await wait();
-        tests.forEach(test => {
-            run.passed(test);
-        });
-        run.end();
-    });
+        const createFilterArg = (item: vscode.TestItem, negate: boolean) => {
+            const fullMatch = item.children.size === 0;
+            const operator = (negate ? "!" : "") + (fullMatch ? "=" : "~");
+            const fullyQualifiedName = item.id.replaceAll(/\(.*\)/g, "");
+            return `FullyQualifiedName${operator}${fullyQualifiedName}`;
+        }
 
-    controller.createRunProfile("Coverage", vscode.TestRunProfileKind.Coverage, async (request, token) => {
-        const run = controller.createTestRun(request, "My test run", true);
-        const wait = () => new Promise(resolve => setTimeout(resolve, 1000));
+        const includeFilters = request.include?.map(item => createFilterArg(item, false));
+        const excludeFilters = request.exclude.map(item => createFilterArg(item, true));
 
-        let tests = request.include ?? controller.items;
-        tests.forEach(test => {
-            run.enqueued(test);
-        })
-        await wait();
-        tests.forEach(test => {
-            run.started(test);
-        })
-        await wait();
-        tests.forEach(test => {
-            run.passed(test);
-        });
-        run.end();
+        const toBeJoined = [...excludeFilters];
+        if (includeFilters) {
+            toBeJoined.push("(" + includeFilters.join("|") + ")")
+        }
+        const joinedFilters = toBeJoined.join("&");
+
+        const filterArgs = joinedFilters.length > 0 ? ["--filter", joinedFilters] : []
+        const resultsFolder = path.join(tmpdir(), await mkdtemp("test-explorer"));
+        const resultsFile = path.join(resultsFolder, "test-results.trx");
+        const loggerArgs = ["--logger", "trx;LogFileName=" + resultsFile]
+
+        try {
+            const output = await new Promise((resolve, reject) => execFile(
+                "dotnet", ["test", ...filterArgs, ...loggerArgs], {
+                env,
+                cwd: vscode.workspace.workspaceFolders[0].uri.fsPath
+            }, (error, stdOut, stdErr) => {
+                // if (error) reject(error);
+                // else
+                resolve(stdOut);
+            }));
+
+            const results = await parseResults(resultsFile);
+
+            for (const result of results) {
+                const parsedName = parseTestName(result.fullName);
+                let item = controller.items.get("");
+                for (const segment of parsedName.segments) {
+                    const segmentString = parsedName.fullName.substring(0, segment.end);
+                    item = item.children.get(segmentString);
+                    if (item === undefined) {
+                        // TODO: need to unfold folded items
+                        console.error("no such test node:", result.fullName, result);
+                        console.error("error at:", segmentString);
+                    }
+                }
+                if (item === undefined) {
+                    console.error("no such test:", result.fullName, result);
+                }
+                if (result.outcome === "Failed")
+                    run.failed(item, { message: result.message });
+                else if (result.outcome === "NotExecuted")
+                    run.skipped(item);
+                else if (result.outcome === "Passed")
+                    run.passed(item);
+                else
+                    console.log("unexpected value for outcome: " + result.outcome);
+            }
+            run.end();
+        }
+        finally {
+            // await unlink(resultsFile);
+            await rmdir(resultsFolder);
+        }
     });
 
     controller.createRunProfile("Debug", vscode.TestRunProfileKind.Debug, async (request, token) => {
